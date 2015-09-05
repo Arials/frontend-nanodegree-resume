802b02db4e79034f1279fd5a703b5c94d197e84a
Adriel Regueira Suárez <adriel@MacBook-Pro-de-Adriel.local>
2015-09-05 11:28:24 +0200

    Add Name and Role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,18 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append("Adriel");
+ //$("#main").append("Adriel");
+ var awesomeThoughts = "I am Adriel and I am AWESOME";
+ console.log(awesomeThoughts);
+
+ var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+ console.log(funThoughts);
+
+ var name = "Adriel Regueira";
+ var role = "IT Enginieer";
+
+ var formattedName = HTMLheaderName.replace("%data%", name);
+ var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+ $("#header").append(formattedName);
+ $("#header").append(formattedRole);
